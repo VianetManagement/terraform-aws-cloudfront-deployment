ac21c35eb3222cf1d26be7e2587fd7507c6e72ca
Noah Jahn <najahn19@gmail.com>
2023-03-02 14:05:44 -0500

    Add server side encryption config to s3 bucket

--- main.tf
+++ main.tf
@@ -67,6 +67,15 @@ resource "aws_s3_bucket" "this" {
       max_age_seconds = cors_rule.value["max_age_seconds"]
     }
   }
+
+  server_side_encryption_configuration {
+    rule {
+      bucket_key_enabled = false
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
 
 #######################################
